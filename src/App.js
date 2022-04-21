e7848e81f47701d80657323c2b96c1d0be6ba77d
Eric Laursen <ericjlaursen@gmail.com>
2022-04-21 15:08:19 -0400

    install more mods

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
       <section>
         <h2>On Semantic-Release Package</h2>
         <ul>
-          <li>Install a package globally: <code>npm i -g semantic-release-cli</code></li>
+          <h3>Install semantic-release</h3>
+          <li>
+            Install a package globally: <code>npm i -g semantic-release-cli</code>
+          </li>
+          <h3>Install commitizen && cz-conventional-changelog</h3>
+          <li>
+            <code>npm i -D commitizen cz-conventional-changelog</code>
+            <p>commitizen allows for writing helpful commit messages. commitizen adds a binary to node_mods dir, `git cz` which will get used in the package.json scripts</p>
+            <p>cz... asks questions to generate a commit message</p>
+          </li>
         </ul>
       </section>
     </div>
